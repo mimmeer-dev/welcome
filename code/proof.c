6ceabedf0077d1eb150bf7c6871cfa4fa5e1e6b5
mimmeer-dev <ajcbpw2@gmail.com>
2026-02-23 08:23:21 +0000

    Create proof.c

---
+++ code/proof.c
@@ -0,0 +1,9 @@
+// Is this good enough proof I am a coder?
+#include <stdio.h>
+
+int main() {
+  printf("Hello, World!\n");
+  int answerToWorld = 1 + 1 // I could do this myself but no
+  printf("1+1=%d\n", answerToWorld);
+  return 0;
+}
